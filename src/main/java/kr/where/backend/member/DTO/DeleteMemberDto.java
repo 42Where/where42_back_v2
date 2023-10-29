26f4f292dbd16b731c84c5f75642512e9c1e3e56
namsizun <namzisun@gmail.com>
2023-10-29 17:20:27 +0900

    [refactor] : delete create method in DeleteMemberDto

--- src/main/java/kr/where/backend/member/DTO/DeleteMemberDto.java
+++ src/main/java/kr/where/backend/member/DTO/DeleteMemberDto.java
@@ -5,12 +5,4 @@ import lombok.Getter;
 @Getter
 public class DeleteMemberDto {
     private Long intraId;
-
-    public static DeleteMemberDto create(final Long intraId) {
-        DeleteMemberDto deleteMemberDto = new DeleteMemberDto();
-
-        deleteMemberDto.intraId = intraId;
-
-        return deleteMemberDto;
-    }
 }
