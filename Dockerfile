5a6e2d9bb1f40cf867c1b43ca275f6c494350320
namsizun <namzisun@gmail.com>
2023-10-30 14:15:16 +0900

    [feat] : add Dockerfile & deploy.sh for CI/CD

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# JDK11 이미지 사용
+FROM openjdk:17-jdk
+
+# VOLUME 설정
+VOLUME /tmp
+
+# Jar파일 위치 변수 정의
+ARG JAR_FILE="build/libs/*.jar"
+
+# Host에 있는 build파일을 Docker 컨테이너에 app.jar로 복사
+COPY ${JAR_FILE} ${JAR_FILE}
+
+# 도커 이미지가 run 될 시에 수행할 동작
+ENTRYPOINT ["nohup", "java", "-jar", "/${JAR_FILE}", "&"]
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# 가동중인 app 도커 중단 및 삭제
+sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
+
+# 기존 이미지 삭제
+sudo docker rmi pgmjun/cicdstudy:latest
+
+# 도커허브 이미지 pull
+sudo docker pull pgmjun/cicdstudy:latest
+
+# 도커 run
+docker run -d -p 8080:8080 --name app pgmjun/cicdstudy:latest
+
+# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
+docker rmi -f $(docker images -f "dangling=true" -q) || true
