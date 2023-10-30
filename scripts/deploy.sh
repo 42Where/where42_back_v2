a1bdd7a99f25c3b7e4162f0c0a2bf27ac89fe476
namsizun <namzisun@gmail.com>
2023-10-30 19:11:44 +0900

    [fix] : change docker_hub_name/repository_name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
 # 기존 이미지 삭제
-sudo docker rmi pgmjun/cicdstudy:latest
+sudo docker rmi where42/where42:latest
 
 # 도커허브 이미지 pull
-sudo docker pull pgmjun/cicdstudy:latest
+sudo docker pull where42/where42:latest
 
 # 도커 run
-docker run -d -p 8080:8080 --name app pgmjun/cicdstudy:latest
+docker run -d -p 8080:8080 --name app where42/where42:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
