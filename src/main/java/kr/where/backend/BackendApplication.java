fa73b32dd30140417f673c4160b49b63d99cee2a
namsizun <namzisun@gmail.com>
2023-11-01 14:42:37 +0900

    [chore] : ci/cd test

--- src/main/java/kr/where/backend/BackendApplication.java
+++ src/main/java/kr/where/backend/BackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("cicd test");
+		System.out.println("for cicd test");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
