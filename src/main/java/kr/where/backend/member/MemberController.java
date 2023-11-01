76cb73d480da0b37d44211e2681ec2a125d6a779
namzisun <namzisun@gmail.com>
2023-11-01 16:21:48 +0900

    Feature/member create (#1)
    * [feat] : Add Member Entity & Create Member API
    
    * [feat] : Add findAllMember API
    
    * [feat] : Add deleteMember API
    
    * [feat] : Add ResponseMemberDto for response body
    
    * [refactor] : delete create method in DeleteMemberDto
    
    * [feat] : add UpdateComment API & UpdateCustomLocation API
    
    * [refactor] : return ResponseEntity

--- src/main/java/kr/where/backend/member/MemberController.java
+++ src/main/java/kr/where/backend/member/MemberController.java
@@ -19,7 +19,6 @@ import kr.where.backend.utils.response.ResponseWithData;
 import kr.where.backend.utils.response.StatusCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
