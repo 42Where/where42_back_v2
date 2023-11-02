01e83eed2e415a8b9c769d9e765c851bfe4952a5
Hanjjong <tim4974@naver.com>
2023-11-03 00:03:55 +0900

    [refactor] : refact to final variable

--- src/main/java/kr/where/backend/group/GroupMemberService.java
+++ src/main/java/kr/where/backend/group/GroupMemberService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import kr.where.backend.group.dto.GroupCreateRequestDTO;
 import kr.where.backend.group.dto.GroupMemberListResponseDTO;
+import kr.where.backend.group.dto.GroupMemberResponseDTO;
 import kr.where.backend.group.entity.Group;
 import kr.where.backend.group.entity.GroupMember;
 import kr.where.backend.member.MemberRepository;
@@ -24,7 +25,7 @@ public class GroupMemberService {
     private final GroupRepository groupRepository;
 
     @Transactional
-    public void createGroupMember(final GroupCreateRequestDTO requestDTO, Long groupId, boolean isOwner){
+    public GroupMemberResponseDTO createGroupMember(final GroupCreateRequestDTO requestDTO, Long groupId, boolean isOwner){
         Group group = groupRepository.findById(groupId)
                 .orElseThrow(() -> new EntityNotFoundException("해당 그룹이 존재하지 않습니다."));
         Member member = memberRepository.findByIntraId(requestDTO.getIntraId())
@@ -32,14 +33,24 @@ public class GroupMemberService {
 
         GroupMember groupMember = new GroupMember(group, member, isOwner);
         groupMemberRepository.save(groupMember);
+
+        GroupMemberResponseDTO createDTO = new GroupMemberResponseDTO(groupId, requestDTO.getGroupName());
+        return createDTO;
     }
 
-    public List<Long> findGroupId(final Long memberId){
-        List<GroupMember> groupMembers = groupMemberRepository.findByMemberIdAndIsOwner(memberId, true);
-        List<Long> collect = groupMembers.stream()
-                .map(m -> m.getGroup().getGroupId())
-                .collect(Collectors.toList());
-        return collect;
+    public List<GroupMemberResponseDTO> findGroupId(final Long memberId){
+        final List<GroupMember> groupMembers = groupMemberRepository.findByMemberIdAndIsOwner(memberId, true);
+
+        final List<GroupMemberResponseDTO> groupMemberResponseDTOS = groupMembers.stream().map(m ->
+            GroupMemberResponseDTO.builder()
+                    .groupId(m.getGroup().getGroupId())
+                    .groupName(m.getGroup().getName()).build()).toList();
+
+//      List<GroupMemberResponseDTO> groupMemberResponseDTOS = groupMembers.stream().map(groupMember -> {
+//            GroupMemberResponseDTO dto = new GroupMemberResponseDTO(groupMember.getGroup().getGroupId(), groupMember.getGroup().getName());
+//            return dto;
+//        }) .collect(Collectors.toList());
+        return groupMemberResponseDTOS;
     }
 
     public List<Long> findGroupMemberId(final Long groupId){
