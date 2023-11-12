b92bfc2d172d1dd205da327154da2e2eecd0a65f
Haeun Jeong <gkdms4248@gmail.com>
2023-11-13 00:30:50 +0900

    feat: add dto for storing json

---
+++ src/main/java/kr/where/backend/api/dto/Cluster.java
@@ -0,0 +1,10 @@
+package kr.where.backend.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class Cluster {
+    String end_at;
+    String begin_at;
+    User user;
+}
---
+++ src/main/java/kr/where/backend/api/dto/Image.java
@@ -0,0 +1,8 @@
+package kr.where.backend.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class Image {
+    private String link;
+}
