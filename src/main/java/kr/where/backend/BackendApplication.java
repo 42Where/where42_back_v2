8c552037608195d16f8a3cfad38def79c58391ea
parksooo <popomarin28@gmail.com>
2023-11-17 16:34:43 +0900

    [chore] : build.gradle insert h2 dependency and [refactor] : remove unused print

--- src/main/java/kr/where/backend/BackendApplication.java
+++ src/main/java/kr/where/backend/BackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("for cicd test");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
