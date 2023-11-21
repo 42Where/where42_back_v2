248535c8d425c500126936281893b3e3232e2e58
Hanjjong <tim4974@naver.com>
2023-11-21 17:51:22 +0900

    [feat] : add Countgroup method

--- src/main/java/kr/where/backend/group/GroupMemberRepository.java
+++ src/main/java/kr/where/backend/group/GroupMemberRepository.java
@@ -25,4 +25,6 @@ public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> 
     boolean existsByGroupAndMember(Group group, Member member);
 
     void deleteAllByGroup_GroupId(Long groupId);
+
+    long countByGroup_GroupIdAndMemberIn(Long groupId, List<Member> members);
 }
