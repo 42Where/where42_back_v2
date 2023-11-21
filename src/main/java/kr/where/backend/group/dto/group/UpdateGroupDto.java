6edb80b661e5dc14154a776a514354d27b7d58ec
Hanjjong <tim4974@naver.com>
2023-11-21 15:02:44 +0900

    [refact] : refact DTO

--- src/main/java/kr/where/backend/group/dto/group/UpdateGroupDto.java
+++ src/main/java/kr/where/backend/group/dto/group/UpdateGroupDto.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
