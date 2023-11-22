18b28bcc7b4d322e3fa6691b1bef745e43709271
Haeun Jeong <gkdms4248@gmail.com>
2023-11-22 20:44:13 +0900

    [fix] : delete secret key parameter

--- src/main/java/kr/where/backend/api/http/HttpRequest.java
+++ src/main/java/kr/where/backend/api/http/HttpRequest.java
@@ -9,10 +9,11 @@ public class HttpRequest {
     private static final String BEARER = "Bearer ";
     private static final String CONTENT_VALUES = "application/json;charset=utf-8";
     private static final String GRANT_TYPE = "authorization_code";
-    private static final String CLIENT_ID = "id";
+    private static final String CLIENT_ID = "id"; // 환경변수
+    private static final String SECRET = "secret"; // 환경변수
     private static final String REDIRECT_UIR = "callbackAddress";
 
-    public static HttpEntity<MultiValueMap<String, String>> requestToken(final String secret, final String code) {
+    public static HttpEntity<MultiValueMap<String, String>> requestToken(final String code) {
         final HttpHeaders headers = new HttpHeaders();
 
         headers.add(HttpHeaders.CONTENT_TYPE, CONTENT_VALUES);
@@ -21,15 +22,14 @@ public class HttpRequest {
 
         params.add("grant_type", GRANT_TYPE);
         params.add("client_id", CLIENT_ID);
-        params.add("client_secret", secret);
+        params.add("client_secret", SECRET);
         params.add("code", code);
         params.add("redirect_uri", REDIRECT_UIR);
 
         return new HttpEntity<>(headers, params);
     }
 
-    public static HttpEntity<MultiValueMap<String, String>> requestAccessToken(final String secret,
-                                                                               final String refreshToken) {
+    public static HttpEntity<MultiValueMap<String, String>> requestAccessToken(final String refreshToken) {
         final HttpHeaders headers = new HttpHeaders();
 
         headers.add(HttpHeaders.CONTENT_TYPE, CONTENT_VALUES);
@@ -38,7 +38,7 @@ public class HttpRequest {
 
         params.add("grant_type", GRANT_TYPE);
         params.add("client_id", CLIENT_ID);
-        params.add("client_secret", secret);
+        params.add("client_secret", SECRET);
         params.add("refresh_token", refreshToken);
 
         return new HttpEntity<>(headers, params);
