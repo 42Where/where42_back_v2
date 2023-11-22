e25beae4c30a7db32d6697388c0db3c971b6c825
Haeun Jeong <gkdms4248@gmail.com>
2023-11-22 22:33:29 +0900

    [fix] : add OAuthToken dto field

--- src/main/java/kr/where/backend/api/mappingDto/OAuthToken.java
+++ src/main/java/kr/where/backend/api/mappingDto/OAuthToken.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 public class OAuthToken {
     private String access_token;
     private String token_type;
-    private String refresh_token;
     private int expires_in;
+    private String refresh_token;
     private String scope;
     private int created_at;
+    private int secret_valid_until;
 }
