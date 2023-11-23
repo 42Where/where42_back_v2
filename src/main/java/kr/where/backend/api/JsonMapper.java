026c77509c9ebd01a1b2062532e03015e9613ec4
parksooo <popomarin28@gmail.com>
2023-11-23 15:13:44 +0900

    [refactor] : apply custom exception

--- src/main/java/kr/where/backend/api/JsonMapper.java
+++ src/main/java/kr/where/backend/api/JsonMapper.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.List;
+import kr.where.backend.exception.json.JsonException;
 import kr.where.backend.exception.json.JsonException.DeserializeException;
 
 public class JsonMapper {
@@ -14,7 +15,7 @@ public class JsonMapper {
         try {
             return OBJECT_MAPPER.readValue(jsonBody, classType);
         } catch (JsonProcessingException e) {
-            throw new DeserializeException();
+            throw new JsonException.DeserializeException();
         }
     }
 
@@ -22,7 +23,7 @@ public class JsonMapper {
         try {
             return Arrays.asList(OBJECT_MAPPER.readValue(jsonBody, classType));
         } catch (JsonProcessingException e) {
-            throw new DeserializeException();
+            throw new JsonException.DeserializeException();
         }
     }
 }
