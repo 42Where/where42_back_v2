3acdc3e4f4a1e4f415f4ac78eee67d38c9ebca22
Haeun Jeong <gkdms4248@gmail.com>
2023-11-23 23:17:03 +0900

    [refactor] : add JsonIgnoreProperties

--- src/main/java/kr/where/backend/api/mappingDto/Cluster.java
+++ src/main/java/kr/where/backend/api/mappingDto/Cluster.java
@@ -1,8 +1,10 @@
 package kr.where.backend.api.mappingDto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Cluster {
     private String  end_at;
     private String begin_at;
