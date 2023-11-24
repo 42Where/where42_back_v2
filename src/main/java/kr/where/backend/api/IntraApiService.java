f69af847228cb2a229ff4d025d56be26898bbbfa
parksooo <popomarin28@gmail.com>
2023-11-24 20:08:56 +0900

    [fix] : apply mapping dto
    [refactor] : change method name

--- src/main/java/kr/where/backend/api/IntraApiService.java
+++ src/main/java/kr/where/backend/api/IntraApiService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import kr.where.backend.api.http.HttpHeader;
 import kr.where.backend.api.http.HttpResponse;
 import kr.where.backend.api.http.UriBuilder;
+import kr.where.backend.api.mappingDto.CadetPrivacy;
 import kr.where.backend.api.mappingDto.Cluster;
 import kr.where.backend.exception.request.RequestException;
-import kr.where.backend.member.DTO.Seoul42;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Recover;
@@ -24,39 +24,39 @@ public class IntraApiService {
      * 본인 정보 반환
      */
     @Retryable
-    public Seoul42 getMeInfo(final String token) {
+    public CadetPrivacy getMyPrivacy(final String token) {
         return JsonMapper
                 .mapping(HttpResponse.getMethod(HttpHeader.request42Info(token),
-                        UriBuilder.me()), Seoul42.class);
+                        UriBuilder.me()), CadetPrivacy.class);
     }
 
     /**
      * 특정 카텟의 정보 반환
      */
     @Retryable
-    public Seoul42 getUserInfo(final String token, final String name) {
+    public CadetPrivacy getCadetPrivacy(final String token, final String name) {
         return JsonMapper
                 .mapping(
                         HttpResponse.getMethod(HttpHeader.request42Info(token), UriBuilder.cadetInfo(name)),
-                        Seoul42.class);
+                        CadetPrivacy.class);
     }
 
     /**
      * index page 별로 image 반환
      */
     @Retryable
-    public List<Seoul42> get42Image(final String token, final int page) {
+    public List<CadetPrivacy> getCadetsImage(final String token, final int page) {
         return JsonMapper
                 .mappings(
                         HttpResponse.getMethod(HttpHeader.request42Info(token), UriBuilder.image(page)),
-                        Seoul42[].class);
+                        CadetPrivacy[].class);
     }
 
     /**
      * 클러스터 아이맥에 로그인 한 카뎃 index page 별로 반환
      */
     @Retryable
-    public List<Cluster> get42ClusterInfo(final String token, final int page) {
+    public List<Cluster> getCadetsInCluster(final String token, final int page) {
         return JsonMapper
                 .mappings(
                         HttpResponse.getMethod(HttpHeader.request42Info(token), UriBuilder.loginCadet(page)),
@@ -67,7 +67,7 @@ public class IntraApiService {
      * 5분 이내 클러스터 아이맥에 로그아웃 한 카뎃 index page 별로 반환
      */
     @Retryable
-    public List<Cluster> get42LocationEnd(final String token, final int page) {
+    public List<Cluster> getLogoutCadetsLocation(final String token, final int page) {
         return JsonMapper
                 .mappings(HttpResponse.getMethod(HttpHeader.request42Info(token),
                                 UriBuilder.loginBeforeFiveMinute(page, false)), Cluster[].class);
@@ -77,7 +77,7 @@ public class IntraApiService {
      * 5분 이내 클러스터 아이맥에 로그인 한 카뎃 index page 별로 반환
      */
     @Retryable
-    public List<Cluster> get42LocationBegin(final String token, final int page) {
+    public List<Cluster> getLoginCadetsLocation(final String token, final int page) {
         return JsonMapper
                 .mappings(HttpResponse.getMethod(HttpHeader.request42Info(token),
                         UriBuilder.loginBeforeFiveMinute(page, true)), Cluster[].class);
@@ -87,26 +87,26 @@ public class IntraApiService {
      * keyWord 부터 end 까지 intra id를 가진 카뎃 10명의 정보 반환
      */
     @Retryable
-    public List<Seoul42> get42UsersInfoInRange(final String token, final String keyWord) {
+    public List<CadetPrivacy> getCadetsInRange(final String token, final String keyWord) {
         return JsonMapper
                 .mappings(HttpResponse.getMethod(
                                 HttpHeader.request42Info(token),
                                 UriBuilder.searchCadets(keyWord, keyWord + END_DELIMITER)),
-                        Seoul42[].class);
+                        CadetPrivacy[].class);
     }
 
     /**
      * 요청 3번 실패 시 실행되는 메서드
      */
     @Recover
-    public Seoul42 fallbackSeoul42(final RuntimeException exception) {
-        log.info("[IntraApiService] Seoul42 fallback {}", exception.getMessage());
+    public CadetPrivacy fallbackCadetPrivacy(final RuntimeException exception) {
+        log.info("[IntraApiService] CadetPrivacy fallback {}", exception.getMessage());
         throw new RequestException.BadRequestException();
     }
 
     @Recover
-    public List<Seoul42> fallbackSeoul42List(final RuntimeException exception) {
-        log.info("[IntraApiService] List<Seoul42> fallback {}", exception.getMessage());
+    public List<CadetPrivacy> fallbackCadetsPrivacy(final RuntimeException exception) {
+        log.info("[IntraApiService] List<CadetPrivacy> fallback {}", exception.getMessage());
         throw new RequestException.BadRequestException();
     }
 
