674aed3bf528228316ae5099915c830f29e3b1b4
parksooo <popomarin28@gmail.com>
2023-11-25 17:23:39 +0900

    [refactor] : add validate digit values, apply lamda, change value name

--- src/main/java/kr/where/backend/search/SearchService.java
+++ src/main/java/kr/where/backend/search/SearchService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.regex.Pattern;
 import kr.where.backend.api.IntraApiService;
 import kr.where.backend.api.mappingDto.CadetPrivacy;
-import kr.where.backend.group.GroupMemberService;
 import kr.where.backend.group.GroupService;
 import kr.where.backend.group.entity.Group;
 import kr.where.backend.member.Member;
@@ -20,22 +19,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-    private static final String PATTERN = "^[a-zA-Z]*$";
+    private static final String PATTERN = "^[0-9a-zA-Z]*$";
     private static final String TOKEN_NAME = "search";
-    private static final int MAXIMUM_SIZE = 9;
+    private static final int MAXIMUM_SIZE = 10;
     private static final int MINIMUM_LENGTH = 2;
     private final MemberService memberService;
     private final IntraApiService intraApiService;
     private final TokenService tokenService;
     private final GroupService groupService;
-    private final GroupMemberService groupMemberService;
 
     /**
-     *
      * @param keyWord 찾을 검색 입력값
-     * @return response로 변경하여 client 측에 전달
-     * 검색하고자 하는 입력값의 결과 10개를 반환
-     * 블랙홀에 빠지지 않은 카뎃을 필터로 걸러서 response DTO 생성
+     * @return response로 변경하여 client 측에 전달 검색하고자 하는 입력값의 결과 10개를 반환 블랙홀에 빠지지 않은 카뎃을 필터로 걸러서 response DTO 생성
+     * 입력 받은 값을 trim으로 공백을 없애주고, 대문자 영어가 들어와도 검색 가능하게 toLowerCase 적용
      */
 
     public List<ResponseSearch> search(final Long intraId, final String keyWord) {
@@ -46,7 +42,7 @@ public class SearchService {
     }
 
     private String validateKeyWord(final String keyWord) {
-        if (keyWord.isEmpty() || !isContainOnlyEnglish(keyWord)) {
+        if (keyWord.isEmpty() || !isContainOnlyEnglishAndDigit(keyWord)) {
             throw new SearchException.InvalidContextException();
         }
         if (!validateLength(keyWord)) {
@@ -55,7 +51,7 @@ public class SearchService {
         return keyWord;
     }
 
-    private boolean isContainOnlyEnglish(final String keyWord) {
+    private boolean isContainOnlyEnglishAndDigit(final String keyWord) {
         return Pattern.matches(PATTERN, keyWord);
     }
 
@@ -70,29 +66,25 @@ public class SearchService {
             isActiveCadet(result,
                     intraApiService
                             .getCadetsInRange(tokenService.findAccessToken(TOKEN_NAME), word));
-        } while(result.size() > MAXIMUM_SIZE);
+        } while (result.size() < MAXIMUM_SIZE);
 
         return result;
     }
 
-    private void isActiveCadet(final List<CadetPrivacy> result, final List<CadetPrivacy> responses) {
-        for (CadetPrivacy response : responses) {
-            if (response.isActive()) {
-                result.add(response);
-            }
-            if (result.size() > MAXIMUM_SIZE) {
-                break;
-            }
-        }
+    private void isActiveCadet(final List<CadetPrivacy> result, final List<CadetPrivacy> cadetPrivacies) {
+        cadetPrivacies.stream()
+                .filter(CadetPrivacy::isActive)
+                .limit(MAXIMUM_SIZE - result.size())
+                .forEach(result::add);
     }
 
-    private List<ResponseSearch> responseOfSearch(final Member member, final List<CadetPrivacy> result) {
+    private List<ResponseSearch> responseOfSearch(final Member member, final List<CadetPrivacy> cadetPrivacies) {
         final Group group = groupService.findOneGroupById(member.getDefaultGroupId());
 
-        return result.stream()
+        return cadetPrivacies.stream()
                 .map(search -> memberService.findOne(search.getId())
-                        .orElse(() -> memberService.createNotMember(search)))
-                .map(ResponseSearch::of)
+                        .orElse(memberService.createFlashMember(search)))
+                .map(search -> ResponseSearch.of(group, search))
                 .toList();
     }
 }
