5432575209acf522dcbe2cd240380c4775e28d38
namzisun <namzisun@gmail.com>
2023-11-27 11:24:16 +0900

    Feature/member create (#22)
    * [feat] : delete no usage dto
    
    * [feat] : update constructor(set location)
    
    * [refactor] : modify create Member(agree / disagree)
    
    * [feat] : add image.version
    
    * [feat] : one-to-one with member
    
    * removed cached
    
    * [chore] : make comment(no usage file)
    
    * [test] : make createForTest method
    
    * [refactor] : change directory(member->api)
    
    * [chore] : make comment(no usage file)
    
    * [chore] : fix a typo
    
    * [chore] : fix a typo
    
    * [test] : modify create-agree-member
    
    * [test] : update create_agree_member_test
    
    * [fix] : update create_disagree_member
    
    * [fix] : done create_member_test
    
    * [fix] : update comment api
    
    * [feat] : delete createMemberDto
    
    * [test] : delete createMemberDto

--- src/main/java/kr/where/backend/api/mappingDto/Hane.java
+++ src/main/java/kr/where/backend/api/mappingDto/Hane.java
@@ -2,6 +2,7 @@ package kr.where.backend.api.mappingDto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
+import org.thymeleaf.standard.expression.IStandardConversionService;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
