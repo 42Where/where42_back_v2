2517758ecfd56338ace2e48308fd6bd466fcb04a
parksooo <popomarin28@gmail.com>
2023-12-05 20:27:44 +0900

    [feat] : create join service and controller

---
+++ src/main/java/kr/where/backend/join/JoinController.java
@@ -0,0 +1,48 @@
+package kr.where.backend.join;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.where.backend.api.exception.JsonException;
+import kr.where.backend.join.dto.ResponseJoin;
+import kr.where.backend.member.exception.MemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v3/join")
+@Tag(name = "join", description = "join API")
+public class JoinController {
+    private final JoinService joinService;
+
+    @Operation(summary = "3.1 JoinMember API", description = "동의 맴버 생성하는 Post API",
+            parameters = {
+                    @Parameter(name = "login",
+                            description = "OAuth2 login한 카뎃의 intra Name", in = ParameterIn.QUERY),
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "맴버 생성 성공",
+                            content = @Content(schema = @Schema(implementation = ResponseJoin.class))),
+                    @ApiResponse(responseCode = "404", description = "맴버 생성 실패",
+                            content = @Content(schema = @Schema(implementation = JsonException.class)))
+            }
+
+    )
+    @PostMapping("/")
+    public ResponseEntity<ResponseJoin> join(@RequestParam("login") final String login) {
+        final ResponseJoin responseJoin = joinService.join(login);
+
+        //프런트 상의 create는 201이까요
+        return ResponseEntity.ok(responseJoin);
+    }
+}
---
+++ src/main/java/kr/where/backend/join/JoinService.java
@@ -0,0 +1,45 @@
+package kr.where.backend.join;
+
+import kr.where.backend.api.HaneApiService;
+import kr.where.backend.api.IntraApiService;
+import kr.where.backend.api.json.CadetPrivacy;
+import kr.where.backend.join.dto.ResponseJoin;
+import kr.where.backend.jwt.JwtService;
+import kr.where.backend.member.MemberService;
+import kr.where.backend.oauthtoken.OauthTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+    private static final String TOKEN_ADMIN = "admin";
+    private static final String TOKEN_HANE = "hane";
+    private final MemberService memberService;
+    private final IntraApiService intraApiService;
+    private final OauthTokenService oauthTokenService;
+    private final HaneApiService haneApiService;
+    private final JwtService jwtService;
+
+    public ResponseJoin join(final String login) {
+
+        final CadetPrivacy cadetPrivacy = intraApiService
+                .getCadetPrivacy(
+                        oauthTokenService.findAccessToken(TOKEN_ADMIN),
+                        login
+                );
+
+        memberService.createAgreeMember(
+                cadetPrivacy,
+                haneApiService
+                        .getHaneInfo(
+                                login, oauthTokenService
+                                        .findAccessToken(TOKEN_HANE))).getId();
+
+        final String accessToken = jwtService.createAccessToken(cadetPrivacy.getId());
+
+        jwtService.create(cadetPrivacy.getId(), jwtService.createRefreshToken(cadetPrivacy.getId()));
+
+        return ResponseJoin.builder().accessToken(accessToken).build();
+    }
+}
