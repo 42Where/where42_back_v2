a46005ab90f105fd0002fdd5316fdd1e7911f6bb
Hanjjong <tim4974@naver.com>
2023-12-11 15:56:04 +0900

    [chore] : modify unused package

--- src/main/java/kr/where/backend/group/GroupController.java
+++ src/main/java/kr/where/backend/group/GroupController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import jakarta.validation.Valid;
 import java.util.List;
 import kr.where.backend.group.dto.groupmember.*;
