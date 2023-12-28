bdf532be9594ec9db9947a1402f7cad468ffa1e4
parksooo <popomarin28@gmail.com>
2023-12-28 17:55:15 +0900

    [fix] : delete hane token values

--- src/main/java/kr/where/backend/join/JoinService.java
+++ src/main/java/kr/where/backend/join/JoinService.java
@@ -18,11 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class JoinService {
-    private static final String TOKEN_ADMIN = "admin";
-    @Value("${hane.token.secret}")
-    private String TOKEN_HANE;
+    private static final String TOKEN_HANE = "hane";
     private final MemberService memberService;
-    private final IntraApiService intraApiService;
     private final OAuthTokenService oAuthTokenService;
     private final HaneApiService haneApiService;
     private final JwtService jwtService;
@@ -30,34 +27,11 @@ public class JoinService {
     @Transactional
     public void join(final Integer intraId) {
 
-//        final CadetPrivacy cadetPrivacy = intraApiService
-//                .getCadetPrivacy(
-//                        "6d77198541c1fdca671c2f683726df5481f8f0c54df93153cefb8fdcff8e9e5c",
-////                        oauthTokenService.findAccessToken(TOKEN_ADMIN),
-//                        login
-//                );
-//
-//        memberService.createAgreeMember(
-//                cadetPrivacy,
-//                haneApiService
-//                        .getHaneInfo(
-//                                login,
-//                                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHRmdW5jIjoiV2hlcmU0MiIsImlhdCI6MTY3NTE3NzIwMCwiZXhwIjoxNzA0MDM4NDAxfQ.qHT9EtQt3fSXRx3UKfljuhSXdGXImhvH45_anKGNWsI"))
-//                .getId();
-//
-//        final String accessToken = jwtService.createAccessToken(cadetPrivacy.getId(), cadetPrivacy.getLogin());
-//
-//        jwtService.create(cadetPrivacy.getId(), jwtService.createRefreshToken(cadetPrivacy.getId(), cadetPrivacy.getLogin()));
-
-//        return ResponseJoin.builder().accessToken(accessToken).build();
         final Member member = memberService.findOne(intraId).orElseThrow(MemberException.NoMemberException::new);
-
-//        memberService.createAgreeMember(CadetPrivacy.createForTest(cadetInfo.getId(), cadetInfo.getLogin(), cadetInfo.getLocation(), cadetInfo.getImage(), cadetInfo.isActive(), cadetInfo.getCreated_at())
-//        ))
         member.setAgree(true);
         member.setInCluster(haneApiService
                         .getHaneInfo(
-                                member.getIntraName(), TOKEN_HANE));
+                                member.getIntraName(), oAuthTokenService.findAccessToken(TOKEN_HANE)));
         final JsonWebToken jsonWebToken = new JsonWebToken(member.getIntraId(), jwtService.createRefreshToken(member.getIntraId(), member.getIntraName()));
         jwtService.save(jsonWebToken);
     }
