326e1f95e7e820c7c2d750d2f4f84fbb4cf939f3
parksooo <popomarin28@gmail.com>
2024-01-06 01:08:00 +0900

    [fix] : fix custom error code

--- src/main/java/kr/where/backend/api/exception/JsonErrorCode.java
+++ src/main/java/kr/where/backend/api/exception/JsonErrorCode.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum JsonErrorCode implements ErrorCode {
-    DESERIALIZE_FAIL(3009, "json 맵핑에 실패 했습니다.");
+    DESERIALIZE_FAIL(1200, "json 맵핑에 실패 했습니다.");
 
     private final int errorCode;
     private final String errorMessage;
--- src/main/java/kr/where/backend/group/exception/GroupErrorCode.java
+++ src/main/java/kr/where/backend/group/exception/GroupErrorCode.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum GroupErrorCode implements ErrorCode {
-    NO_GROUP(2000, "그룹이 존재하지 않습니다."),
-    DUPLICATED_GROUP_NAME(2001, "이미 존재하는 그룹 이름입니다.");
+    NO_GROUP(1100, "그룹이 존재하지 않습니다."),
+    DUPLICATED_GROUP_NAME(1101, "이미 존재하는 그룹 이름입니다.");
 
     private final int errorCode;
     private final String errorMessage;
