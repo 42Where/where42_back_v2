a500a2ef5b6b92ce40813d33f4731fc854b03d71
parksooo <popomarin28@gmail.com>
2024-01-09 16:38:11 +0900

    [feat] : redirect 42

--- src/main/java/kr/where/backend/auth/oauth2login/OAuth2SuccessHandler.java
+++ src/main/java/kr/where/backend/auth/oauth2login/OAuth2SuccessHandler.java
@@ -67,7 +67,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                         request,
                         response,
                         UriComponentsBuilder
-                                .fromUriString("http://13.209.149.15:8080/")
+                                .fromUriString("http://13.209.149.15/login-sucess")
                                 .queryParam("token", accessToken)
                                 .queryParam("intraId",cadetInfo.getId())
                                 .queryParam("agreement", isAgree)
