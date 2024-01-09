1e5eeed5748c2f7db461b3eddb53152f1fb658c7
parksooo <popomarin28@gmail.com>
2024-01-09 21:52:34 +0900

    [feat] : add constructor

--- src/main/java/kr/where/backend/group/dto/group/UpdateGroupDTO.java
+++ src/main/java/kr/where/backend/group/dto/group/UpdateGroupDTO.java
@@ -1,9 +1,11 @@
 package kr.where.backend.group.dto.group;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateGroupDTO {
     @NotBlank
     private Long groupId;
