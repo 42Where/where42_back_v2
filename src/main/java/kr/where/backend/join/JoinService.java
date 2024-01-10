143ed59caccc356f30d6ec43d4df52adda6d11f2
parksooo <popomarin28@gmail.com>
2024-01-10 17:16:16 +0900

    [fix] : use hane real token

--- src/main/java/kr/where/backend/join/JoinService.java
+++ src/main/java/kr/where/backend/join/JoinService.java
@@ -38,7 +38,7 @@ public class JoinService {
 //                                member.getIntraName(), oAuthTokenService.findAccessToken(TOKEN_HANE)));
         member.setInCluster(haneApiService
                         .getHaneInfo(
-                                member.getIntraName(), TOKEN_HANE));
+                                member.getIntraName(), haneToken));
 
         ResponseGroupDTO responseGroupDto = groupService.createGroup(new CreateGroupDTO(Group.DEFAULT_GROUP), authUser);
         member.setDefaultGroupId(responseGroupDto.getGroupId());
