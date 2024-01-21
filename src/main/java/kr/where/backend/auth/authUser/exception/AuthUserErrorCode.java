810ef8062c860a52d28a37484a63a4cf59e4f6cb
parksooo <popomarin28@gmail.com>
2024-01-21 16:29:01 +0900

    [feat] : create AuthUser exception handle

---
+++ src/main/java/kr/where/backend/auth/authUser/exception/AuthUserErrorCode.java
@@ -0,0 +1,14 @@
+package kr.where.backend.auth.authUser.exception;
+
+import kr.where.backend.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AuthUserErrorCode implements ErrorCode {
+    FORBIDDEN_USER(1800, "Where42 서비스에 동의한 user가 아닙니다.");
+
+    private final int errorCode;
+    private final String errorMessage;
+}
