3d8fa8589fe44393bd4b1e99ad5452c836d5fece
parksooo <popomarin28@gmail.com>
2024-01-29 16:38:36 +0900

    [feat] : add ANONYMOUS_USER errorCode

--- src/main/java/kr/where/backend/auth/authUser/exception/AuthUserErrorCode.java
+++ src/main/java/kr/where/backend/auth/authUser/exception/AuthUserErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AuthUserErrorCode implements ErrorCode {
-    FORBIDDEN_USER(1800, "Where42 서비스에 동의한 user가 아닙니다.");
+    FORBIDDEN_USER(1800, "Where42 서비스에 동의한 user가 아닙니다."),
+    ANONYMOUS_USER(1801, "인가 인증을 받지 않은 user 입니다");
 
     private final int errorCode;
     private final String errorMessage;
