af4c6930b3c10a53d9737816842d59c5386763c3
parksooo <popomarin28@gmail.com>
2024-01-30 18:59:05 +0900

    [feat] : feat Cookie bakery to set Cookie

---
+++ src/main/java/kr/where/backend/auth/oauth2login/cookie/CookieShop.java
@@ -0,0 +1,21 @@
+package kr.where.backend.auth.oauth2login.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieShop {
+    public static void bakedCookie(
+            final HttpServletResponse response,
+            final String key,
+            final int expiry,
+            final String token) {
+        final Cookie cookie = new Cookie(key, token);
+
+        cookie.setMaxAge(expiry);
+        cookie.setPath("/");
+//        cookie.setSecure(true);
+//        cookie.setHttpOnly(true);
+
+        response.addCookie(cookie);
+    }
+}
