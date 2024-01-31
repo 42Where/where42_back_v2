496007fc3060098c073d90f62774f15e534bead2
parksooo <popomarin28@gmail.com>
2024-01-31 15:42:15 +0900

    [swagger] : apply Api interface

--- src/main/java/kr/where/backend/join/JoinController.java
+++ src/main/java/kr/where/backend/join/JoinController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import kr.where.backend.api.exception.JsonException;
 import kr.where.backend.auth.authUser.AuthUser;
 import kr.where.backend.auth.authUser.AuthUserInfo;
-import kr.where.backend.join.dto.ResponseJoinDTO;
+import kr.where.backend.jwt.dto.ResponseRefreshTokenDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +17,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v3/join")
-@Tag(name = "join", description = "join API")
 public class JoinController {
     private final JoinService joinService;
 
-    @Operation(summary = "3.1 JoinMember API", description = "동의 맴버 생성하는 Post API",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "맴버 생성 성공",
-                            content = @Content(schema = @Schema(implementation = ResponseJoinDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "맴버 생성 실패",
-                            content = @Content(schema = @Schema(implementation = JsonException.class)))
-            }
-
-    )
     @PostMapping("")
-    public ResponseEntity<ResponseJoinDTO> join(
-            final HttpServletRequest request,
-            @AuthUserInfo final AuthUser authUser) {
+    public ResponseEntity<ResponseRefreshTokenDTO> join(@AuthUserInfo final AuthUser authUser) {
 
         return ResponseEntity.ok(joinService.join(authUser));
     }
