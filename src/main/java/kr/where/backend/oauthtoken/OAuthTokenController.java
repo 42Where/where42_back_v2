25c8e0b9c9f527ad69c8a77261ba8621adc9ab29
hani <gkdms4248@gmail.com>
2024-02-02 17:11:12 +0900

    [comment] : add comment

--- src/main/java/kr/where/backend/oauthtoken/OAuthTokenController.java
+++ src/main/java/kr/where/backend/oauthtoken/OAuthTokenController.java
@@ -8,6 +8,9 @@ import kr.where.backend.api.json.CadetPrivacy;
 import kr.where.backend.api.json.Cluster;
 import kr.where.backend.api.json.OAuthTokenDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v3/token")
 @RequiredArgsConstructor
@@ -25,38 +29,51 @@ public class OAuthTokenController {
     private final TokenApiService tokenApiService;
     private final IntraApiService intraApiService;
 
-    // test 용
-
     /**
-     * REDIRECT_URI http://localhost:8080/v3/token
+     * OAuth 인증 후 해당 uri 로 code 반환됨
      */
     @GetMapping("")
-    public String createAccessToken(@RequestParam("code") String code) {
-        System.out.println(code);
-        return code;
+    public void createAccessToken(@RequestParam("code") String code) {
+        log.info("code : {}", code);
     }
 
-    // 프론트가 없어서 위에서 받은 code 를 수동으로 넣어줘야 함
+    /**
+     * code로 OAuth token 받아와서 생성 & 업데이트
+     * @param name : 생성할 token 이름
+     * @param code : log에 찍힌 code
+     */
     @PostMapping("")
-    public String createToken(@RequestParam("name") String name, @RequestParam("code") String code) {
+    public ResponseEntity createToken(@RequestParam("name") String name, @RequestParam("code") String code) {
         final OAuthTokenDto oAuthToken = tokenApiService.getOAuthToken(code);
         oauthTokenService.createToken(name, oAuthToken);
-        return oAuthToken.getAccess_token();
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    /**
+     * hane token 생성 & 업데이트
+     * @param token : hane token
+     */
+    @PostMapping("/update/hane")
+    public ResponseEntity updateHaneToken(@RequestParam("code") final String token) {
+        oauthTokenService.updateHaneToken(token);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /**
+     * @param name : 삭제할 token 이름
+     */
     @DeleteMapping("")
-    public void deleteToken(@RequestParam("name") final String name) {
+    public void deleteToken(@RequestParam("name") String name) {
         oauthTokenService.deleteToken(name);
     }
 
-    @PostMapping("/update/hane")
-    public String updateHaneToken(@RequestParam("code") final String token) {
-        oauthTokenService.updateHaneToken(token);
-        return token;
-    }
 
-    // api test
 
+
+    /**
+     * api test
+     * OAuthToken 넣고 모든 api 호출 성공하면 삭제
+     */
 //    @GetMapping("/user")
 //    public CadetPrivacy getUserInfo() {
 //        final String accessToken = oauthTokenService.findAccessToken("test");
