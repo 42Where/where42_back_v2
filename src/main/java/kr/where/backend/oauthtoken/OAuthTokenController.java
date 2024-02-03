4191122b94c1062e8a1caa47ac3cad178027673e
hani <gkdms4248@gmail.com>
2024-02-03 19:53:50 +0900

    [swagger] : add swagger hidden annotation

--- src/main/java/kr/where/backend/oauthtoken/OAuthTokenController.java
+++ src/main/java/kr/where/backend/oauthtoken/OAuthTokenController.java
@@ -1,5 +1,6 @@
 package kr.where.backend.oauthtoken;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import kr.where.backend.api.IntraApiService;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v3/token")
 @RequiredArgsConstructor
-@Tag(name = "oauth", description = "oauth token API")
+@Hidden
 public class OAuthTokenController {
 
     private final OAuthTokenService oauthTokenService;
