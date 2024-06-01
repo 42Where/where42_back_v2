8e68685bd2add81c9584343eb7f5b5d049372386
parksooo <popomarin28@gmail.com>
2024-06-01 16:07:36 +0900

    [feat] : create NotFromSeoulCampus exception code

--- src/main/java/kr/where/backend/member/exception/MemberErrorCode.java
+++ src/main/java/kr/where/backend/member/exception/MemberErrorCode.java
@@ -9,7 +9,8 @@ import lombok.Getter;
 public enum MemberErrorCode implements ErrorCode {
 
 	NO_MEMBER(1000, "존재하지 않는 맴버입니다."),
-	DUPLICATED_MEMBER(1001, "이미 존재하는 맴버입니다.");
+	DUPLICATED_MEMBER(1001, "이미 존재하는 맴버입니다."),
+	NOT_FROM_SEOUL_CADET(1002, "서울 캠퍼스 카뎃이 아닙니다.");
 
 	private final int errorCode;
 	private final String errorMessage;
