3e753fb35e7ca16234767fda41325cb4ce0955c5
parksooo <popomarin28@gmail.com>
2024-06-04 17:39:08 +0900

    [feat] : apply local time

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ sudo echo $PASSWORD | docker login -u $USERNAME --password-stdin
 sudo docker pull where42/where42:latest
 
 # 도커 run
-docker run -d -p 8080:8080 -v /home/ec2-user:/config --name app where42/where42:latest
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name app where42/where42:latest -e TZ=Asia/Seoul
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
