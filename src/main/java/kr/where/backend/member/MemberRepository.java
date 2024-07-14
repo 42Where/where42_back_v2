2e809b7e1cf0633c97cba0571d06890aeef70651
parksooo <popomarin28@gmail.com>
2024-07-14 20:47:08 +0900

    [feat] : to get member of name in DTO who argree where42 service

--- src/main/java/kr/where/backend/member/MemberRepository.java
+++ src/main/java/kr/where/backend/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package kr.where.backend.member;
 
+import kr.where.backend.api.json.hane.HaneRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<List<Member>> findByIntraIdIn(List<Integer> intraId);
 
 	Optional<List<Member>> findAllByAgreeTrue();
+
+	@Query("select new kr.where.backend.api.json.hane.HaneRequestDto(m.intraName) "
+			+ "from Member m where m.agree is true ")
+	Optional<List<HaneRequestDto>> findAllToUseHaneApi();
 }
