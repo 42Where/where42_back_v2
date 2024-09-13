c396957f715cfe0ab120a13a098c0d7065554acd
ilko <114412252+iko-ilko@users.noreply.github.com>
2024-09-13 15:01:00 +0900

    [feat] : VersionService temp backup

--- src/main/java/kr/where/backend/version/VersionService.java
+++ src/main/java/kr/where/backend/version/VersionService.java
@@ -26,8 +26,8 @@ public class VersionService {
         // 보안측면(request가 예상 외 값이 들어왔을 때)에서 봤을 땐 없는 os가 들어오면 예외를 던지는게 아니라 enum[IOS, ANDROID]으로 체크해서 정해진 os만 유효하게끔.
         final Version version = versionRepository.findByOsType(checkVersionDTO.getOs())
                         .orElse(new Version(checkVersionDTO.getVersion(), checkVersionDTO.getOs()));
-        // 커스텀익셉션 나중에 작성할 것임.
-        //유효성 체크
+        //버전 값 체크
+        checkVersion(version);
 
 
         version.setLatestVersion(checkVersionDTO.getVersion());
@@ -40,4 +40,10 @@ public class VersionService {
 
 
     //
+
+    private void checkVersion(Version version) {
+        int[] each = version.getLatestVersion().split("\\.");
+
+        string
+    }
 }
