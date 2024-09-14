fc99b19ac2d6568a1dd8bcc2a37917732ad396cb
ilko <114412252+iko-ilko@users.noreply.github.com>
2024-09-14 19:49:33 +0900

    [feat] : init endpoint, [comment] : modify docs style

--- src/main/java/kr/where/backend/version/VersionController.java
+++ src/main/java/kr/where/backend/version/VersionController.java
@@ -3,6 +3,7 @@ package kr.where.backend.version;
 import kr.where.backend.version.dto.CheckVersionDTO;
 import kr.where.backend.version.dto.ResponseVersionDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +20,17 @@ public class VersionController {
         return ResponseEntity.ok(
                 versionService.checkVersion(checkVersionDTO)
         );
+    }
+
+    /**
+     * 서버에 저장된 Enum[IOS, ANDROID]을 기준으로 DB에 저장.
+     * 버전은 0.0.0 강제 초기화
+     *  - 추후에 각 os에서 checkVersion 호출 시 업데이트 가능함.
+     * @return ResponseEntity.status(HttpStatus.OK).build()
+     */
+    @GetMapping("/init")
+    public ResponseEntity<String> initVersion() {
+        versionService.init();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
