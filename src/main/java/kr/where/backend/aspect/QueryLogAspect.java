ab08562e80f1e74475743d667e3aa204c01634ee
parksooo <popomarin28@gmail.com>
2024-11-14 19:44:36 +0900

    [feat] : query log speed rate aop

--- src/main/java/kr/where/backend/aspect/QueryLogAspect.java
+++ src/main/java/kr/where/backend/aspect/QueryLogAspect.java
@@ -1,10 +1,15 @@
 package kr.where.backend.aspect;
 
+import kr.where.backend.auth.authUser.AuthUser;
+import kr.where.backend.auth.authUser.AuthUserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
@@ -12,14 +17,25 @@ import org.springframework.stereotype.Component;
 public class QueryLogAspect {
 
     @Around("execution(* kr.where.backend..*Repository.*(..))")
-    public Object measureJpaExecutionTime(final ProceedingJoinPoint point) throws Throwable{
+    public Object measureJpaExecutionTime(final ProceedingJoinPoint point) throws Throwable {
+        final ServletRequestAttributes attributes
+                = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         final long startTime = System.currentTimeMillis();
 
-        Object result = point.proceed();
+        final Object result = point.proceed();
         final long executionTime = System.currentTimeMillis() - startTime;
+        final AuthUser authUser = (AuthUser) SecurityContextHolder.getContext().getAuthentication();
 
-        log.info("Jpa query execution time : {}ms, doing method : {}", executionTime, point.getSignature());
-
+        if (attributes != null) {
+            log.info("Ip : {}, Request URL : {}, Request Method : {}, UserId : {}, Msg : {}, Execute Method : {}",
+                    attributes.getRequest().getRemoteAddr(),
+                    attributes.getRequest().getRequestURL().toString(),
+                    attributes.getRequest().getMethod(),
+                    authUser.getIntraId(),
+                    "Query Execute time : " + executionTime + "ms",
+                    point.getSignature()
+            );
+        }
         return result;
     }
 }
