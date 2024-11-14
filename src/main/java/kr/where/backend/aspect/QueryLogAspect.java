242f0a162fe8830356942bf9368137a5f12ab2ba
parksooo <popomarin28@gmail.com>
2024-11-14 20:36:05 +0900

    [fix] : exclude repository

--- src/main/java/kr/where/backend/aspect/QueryLogAspect.java
+++ src/main/java/kr/where/backend/aspect/QueryLogAspect.java
@@ -1,7 +1,6 @@
 package kr.where.backend.aspect;
 
 import kr.where.backend.auth.authUser.AuthUser;
-import kr.where.backend.auth.authUser.AuthUserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -16,7 +15,10 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Slf4j
 public class QueryLogAspect {
 
-    @Around("execution(* kr.where.backend..*Repository.*(..))")
+    @Around("execution(* kr.where.backend..*Repository.*(..)) &&" +
+            "!execution(* kr.where.backend.member.MemberRepository.findByIntraId(..)) &&" +
+            "!execution(* kr.where.backend.member.MemberRepository.save(..)) &&" +
+            "!execution(* kr.where.backend.location.LocationRepository.save(..))")
     public Object measureJpaExecutionTime(final ProceedingJoinPoint point) throws Throwable {
         final ServletRequestAttributes attributes
                 = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
@@ -24,7 +26,7 @@ public class QueryLogAspect {
 
         final Object result = point.proceed();
         final long executionTime = System.currentTimeMillis() - startTime;
-        final AuthUser authUser = (AuthUser) SecurityContextHolder.getContext().getAuthentication();
+        final AuthUser authUser = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         if (attributes != null) {
             log.info("Ip : {}, Request URL : {}, Request Method : {}, UserId : {}, Msg : {}, Execute Method : {}",
