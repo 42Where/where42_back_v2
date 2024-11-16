8daa096bbeb034b597d7bac2ee9b317393953e94
parksooo <popomarin28@gmail.com>
2024-11-16 16:28:09 +0900

    [delete] : apply deleting queryLog annotation

--- src/main/java/kr/where/backend/location/LocationRepository.java
+++ src/main/java/kr/where/backend/location/LocationRepository.java
@@ -1,6 +1,5 @@
 package kr.where.backend.location;
 
-import kr.where.backend.aspect.QueryLog;
 import kr.where.backend.member.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-	@QueryLog
 	Location findByMember(Member member);
 }
--- src/main/java/kr/where/backend/version/VersionRepository.java
+++ src/main/java/kr/where/backend/version/VersionRepository.java
@@ -1,6 +1,5 @@
 package kr.where.backend.version;
 
-import kr.where.backend.aspect.QueryLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface VersionRepository extends JpaRepository<Version, Long> {
-    @QueryLog
     Optional<Version> findByOsType(String osType);
 }
