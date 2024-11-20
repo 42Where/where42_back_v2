a3f2154497b3b86a6e69dbb2de4a5009dcc11276
parksooo <popomarin28@gmail.com>
2024-11-20 22:30:08 +0900

    [feat] : add promtail container

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,25 @@
 # 가동중인 app 도커 중단 및 삭제
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
+sudo docker ps -a -q --filter "name=promtail" | grep -q . && docker stop promtail && docker rm promtail || true
+
 # 기존 이미지 삭제
 sudo docker rmi where42/where42:latest
 
 sudo echo $PASSWORD | docker login -u $USERNAME --password-stdin
+
 # 도커허브 이미지 pull
 sudo docker pull where42/where42:latest
 
 # 도커 run
 docker run -d -p 8080:8080 -v /home/ec2-user:/config --name app where42/where42:latest -e TZ=Asia/Seoul
+
+docker run -d \
+ --name promtail \
+ -v /home/ec2-user/logs:logs \
+ -v /home/ec2-user/promtail-config.yml:/etc/promtail/config.yml \
+ grafana/promtail:latest \
+ -config.file=/etc/promtail/config.yml
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
