4c90eb59cf3b54f8ca5e958b72111acbc27530a8
soohalee <suha630@naver.com>
2024-11-23 20:09:45 +0900

    [fix] : 공지 서비스 테스트 중복되는 초기화 코드 @beforeEach 로 분리함.

--- src/test/java/kr/where/backend/announcement/AnnouncementServiceTest.java
+++ src/test/java/kr/where/backend/announcement/AnnouncementServiceTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import kr.where.backend.announcement.dto.DeleteAnnouncementDto;
 import kr.where.backend.announcement.exception.AnnouncementException;
 import kr.where.backend.auth.authUser.AuthUser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,24 +23,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @SpringBootTest
 public class AnnouncementServiceTest {
-    @Autowired
-    private AnnouncementService announcementService;
 
     @Autowired
+    private AnnouncementService announcementService;
+    @Autowired
     private AnnouncementRepository announcementRepository;
+    private AuthUser authUser;
+
+    @BeforeEach
+    public void setUP() {
+        Collection<? extends GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("user"));
+        authUser = new AuthUser(135436, "soohlee", 1L);
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(authUser, "", authorities));
+    }
+
     @DisplayName("공지를 삭제 실패하는 기능 테스트")
     @Test
     @Rollback
     void deleteAnnouncementTest() {
         //given
-        Collection<? extends GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("user"));
-        AuthUser authUser = new AuthUser(135436, "soohlee", 1L);
-        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(authUser, "", authorities));
-        announcementRepository.save(new Announcement("a", "a", "a", LocalDate.now(), LocalDate.now()));
-
-        //when
+        announcementRepository.save(new Announcement("점검 공지", "오늘 10시 ~ 12시까지 점검입니다.", "soohlee", LocalDate.now(), LocalDate.now()));
         DeleteAnnouncementDto deleteAnnouncementDto = new DeleteAnnouncementDto(1L);
 
+        //when
         //then
         assertThatThrownBy(() -> announcementService.delete(deleteAnnouncementDto, authUser)).
                 isInstanceOf(AnnouncementException.NoAnnouncementException.class);
