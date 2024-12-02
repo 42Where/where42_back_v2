45f0b028148691d44b91e55413b9b69aa6948898
soohalee <suha630@naver.com>
2024-12-02 11:28:43 +0900

    [fix] : add notBlank annotation in RequestAdminStatusDTO

--- src/main/java/kr/where/backend/admin/dto/RequestAdminStatusDTO.java
+++ src/main/java/kr/where/backend/admin/dto/RequestAdminStatusDTO.java
@@ -1,10 +1,12 @@
 package kr.where.backend.admin.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class RequestAdminStatusDTO {
+    @NotBlank
     private final String role;
 }
