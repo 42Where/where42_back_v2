918a66a80472007ce3a9410d8ea9e41434f745d2
parksooo <popomarin28@gmail.com>
2024-12-17 17:38:37 +0900

    [test] : 자신의 imac 자리 사용 기록을 생성하고 넣어주는 test

---
+++ src/test/java/kr/where/backend/seatHistory/SeatHistoryTest.java
@@ -0,0 +1,60 @@
+package kr.where.backend.seatHistory;
+
+import com.fasterxml.classmate.MemberResolver;
+import kr.where.backend.api.json.CadetPrivacy;
+import kr.where.backend.api.json.hane.Hane;
+import kr.where.backend.member.Member;
+import kr.where.backend.member.MemberRepository;
+import kr.where.backend.member.MemberService;
+import kr.where.backend.member.exception.MemberException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class SeatHistoryTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    SeatHistoryRepository seatHistoryRepository;
+
+    @Autowired
+    SeatHistoryService seatHistoryService;
+    private final static Integer CAMPUS_ID = 29;
+    @BeforeEach
+    void setUp() {
+        CadetPrivacy cadetPrivacy = new CadetPrivacy(11111, "suhwpark", "c1r1s1", "image", true, "2022-10-31", CAMPUS_ID);
+        Hane hane = Hane.create("IN");
+        Member member = memberService.createAgreeMember(cadetPrivacy, hane);
+    }
+
+    @Test
+    @DisplayName("맴버가 자리에 로그인 했을 때, 로그인 한 자리 정보 추가")
+    void createSeatHistory() {
+        //given
+        String seat = "c1r2s1";
+
+        //when
+        Long id = seatHistoryService(seat, 111111);
+
+        //then
+        SeatHistory seatHistory = seatHistoryRepository.findById(id);
+        List<SeatHistory> seatHistoryList = member.getSeatHistory();
+        assertThat(seatHistoryList.contains(seatHistory)).isTrue();
+    }
+}
