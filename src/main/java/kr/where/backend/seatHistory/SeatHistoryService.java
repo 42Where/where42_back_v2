c04caa47b788e8f1933f48f601513b9ed7523b7e
parksooo <popomarin28@gmail.com>
2024-12-17 18:59:19 +0900

    [test] : 생성 로직

---
+++ src/main/java/kr/where/backend/seatHistory/SeatHistoryService.java
@@ -0,0 +1,27 @@
+package kr.where.backend.seatHistory;
+
+import kr.where.backend.member.Member;
+import kr.where.backend.member.MemberRepository;
+import kr.where.backend.member.exception.MemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SeatHistoryService {
+    private final SeatHistoryRepository seatHistoryRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long create(final String seat, final Integer intraId) {
+        final Member member = memberRepository.findByIntraId(intraId)
+                .orElseThrow(MemberException.NoMemberException::new);
+
+        final SeatHistory seatHistory = new SeatHistory(seat, member);
+        member.getSeatHistories().add(seatHistory);
+
+        return seatHistory.getId();
+    }
+}
