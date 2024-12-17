5e400a842f33cee53eb6e680d2162044855f98f4
parksooo <popomarin28@gmail.com>
2024-12-17 17:46:46 +0900

    [feat] : 엔티티 구성 요소 및 연관 관계 설정

--- src/main/java/kr/where/backend/seatHistory/SeatHistory.java
+++ src/main/java/kr/where/backend/seatHistory/SeatHistory.java
@@ -1,7 +1,25 @@
 package kr.where.backend.seatHistory;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import kr.where.backend.member.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String imac;
+
+    private Long count;
+
+    @ManyToOne
+    @JoinColumn(name = "intra_id")
+    private Member member;
 }
