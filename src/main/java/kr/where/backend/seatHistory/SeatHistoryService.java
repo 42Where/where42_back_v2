de8f36ebced78ef535d05da0f7bd07f0ff3e2be3
parksooo <popomarin28@gmail.com>
2024-12-18 14:29:21 +0900

    [feat] : 사용한 자리 기록시, 정보가 없는 새로운 자리라면 생성 후 기록, 아니라면 기존의 정보에 사용수 증가

--- src/main/java/kr/where/backend/seatHistory/SeatHistoryService.java
+++ src/main/java/kr/where/backend/seatHistory/SeatHistoryService.java
@@ -1,11 +1,14 @@
 package kr.where.backend.seatHistory;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import kr.where.backend.member.Member;
 import kr.where.backend.member.MemberRepository;
 import kr.where.backend.member.exception.MemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,16 @@ public class SeatHistoryService {
 
         member.getSeatHistories().add(seatHistory);
         return seatHistory.getId();
+    }
+
+    @Transactional
+    public void report(final String seat, final Integer intraId) {
+        final Member member = memberRepository.findByIntraId(intraId)
+                .orElseThrow(MemberException.NoMemberException::new);
+        member.getSeatHistories()
+                .stream()
+                .filter(s -> Objects.equals(s.getImac(), seat))
+                .findFirst()
+                .ifPresentOrElse(SeatHistory::increaseCount, () -> create(seat, intraId));
     }
 }
