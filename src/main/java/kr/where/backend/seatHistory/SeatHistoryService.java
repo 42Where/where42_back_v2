7fe9808f4ff213c8cb410416463d52f2e7fd4aa5
parksooo <popomarin28@gmail.com>
2024-12-18 17:21:30 +0900

    [feat] : 가장 많이 앉은 자리 top3 조회 서비스

--- src/main/java/kr/where/backend/seatHistory/SeatHistoryService.java
+++ src/main/java/kr/where/backend/seatHistory/SeatHistoryService.java
@@ -1,13 +1,15 @@
 package kr.where.backend.seatHistory;
 
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import kr.where.backend.member.Member;
 import kr.where.backend.member.MemberRepository;
 import kr.where.backend.member.exception.MemberException;
+import kr.where.backend.seatHistory.dto.ResponseHistoryDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -38,5 +40,18 @@ public class SeatHistoryService {
                 .filter(s -> Objects.equals(s.getImac(), seat))
                 .findFirst()
                 .ifPresentOrElse(SeatHistory::increaseCount, () -> create(seat, intraId));
+    }
+
+    public List<ResponseHistoryDTO> getPopularSeatHistory(final Integer intraId) {
+        final List<SeatHistory> seatHistories = seatHistoryRepository
+                .findSeatHistoriesByMemberASCLimit(intraId, PageRequest.of(0, 3));
+        final int size = seatHistoryRepository.sumSeatHistoriesCountByMember(intraId);
+
+        return seatHistories.stream()
+                .map(s -> new ResponseHistoryDTO(
+                        s.getImac(),
+                        Math.round(((double) s.getCount() / size) * 100 * 10.0) / 10.0)
+                )
+                .toList();
     }
 }
