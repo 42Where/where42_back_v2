58ea56fb4cd6e78ccf1a0abcfc364650b585f50a
soohalee <suha630@naver.com>
2024-12-19 17:37:45 +0900

    [refactor] : Remove unused function

--- src/main/java/kr/where/backend/cluster/ClusterRepository.java
+++ src/main/java/kr/where/backend/cluster/ClusterRepository.java
@@ -1,17 +1,11 @@
 package kr.where.backend.cluster;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ClusterRepository extends JpaRepository<Cluster, Long> {
-    Optional<Cluster> findByClusterAndRowIndexAndSeat(String c, Integer r, Integer s);
-
-    @Modifying
-    @Query("UPDATE Cluster c SET c.member = null")
-    void updateAllMembersToNull();
+    Optional<Cluster> findByClusterAndRowIndexAndSeat(final String c, final Integer r, final Integer s);
 }
