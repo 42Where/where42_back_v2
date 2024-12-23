1f51dd808467c97d37abbb037cbe5e606f0f7282
Hanjjong <tim4974@naver.com>
2024-12-23 21:36:21 +0900

    [feat] : feat getPopularSeat API controller

--- src/main/java/kr/where/backend/cluster/ClusterController.java
+++ src/main/java/kr/where/backend/cluster/ClusterController.java
@@ -3,6 +3,7 @@ package kr.where.backend.cluster;
 import kr.where.backend.auth.authUser.AuthUser;
 import kr.where.backend.auth.authUser.AuthUserInfo;
 import kr.where.backend.cluster.dto.ResponseClusterListDTO;
+import kr.where.backend.cluster.dto.ResponseMostPopularSeatDTO;
 import kr.where.backend.cluster.swagger.ClusterApiDocs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,5 +31,10 @@ public class ClusterController implements ClusterApiDocs {
     public ResponseEntity<ResponseClusterListDTO> getCluster(@PathVariable("cluster") final String cluster,
                                                              @AuthUserInfo final AuthUser authUser) {
         return ResponseEntity.ok(clusterService.getLoginMember(authUser, cluster));
+    }
+
+    @GetMapping("/popular")
+    public ResponseEntity<ResponseMostPopularSeatDTO> getMostPopularSeat() {
+        return ResponseEntity.ok(clusterService.getMostPopularSeat());
     }
 }
