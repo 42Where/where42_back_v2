4405ae3fec8a66c5008a5ec1e2d97dac9b295c1f
parksooo <popomarin28@gmail.com>
2024-12-25 18:52:17 +0900

    [fix] : 로그아웃 시 accessToken 추출하여, 무효화 시키는 로직 추가

--- src/main/java/kr/where/backend/logout/LogoutService.java
+++ src/main/java/kr/where/backend/logout/LogoutService.java
@@ -1,6 +1,8 @@
 package kr.where.backend.logout;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kr.where.backend.auth.authUser.AuthUser;
+import kr.where.backend.jwt.JwtService;
 import kr.where.backend.redisToken.RedisTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,11 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogoutService {
     private final RedisTokenService redisTokenService;
-    public String logout(final AuthUser authUser) {
-        //여기서 블랙리스트 추가 해야할 듯 합니다.
-        redisTokenService.saveInBlackList(authUser.getIntraId().toString());
-        redisTokenService.deleteTokens(authUser.getIntraId().toString());
+    private final JwtService jwtService;
+
+    public Integer logout(final HttpServletRequest request, final Integer intraId) {
+        final String accessToken = jwtService.extractToken(request)
+                .orElse(null);
+        redisTokenService.invalidateToken(accessToken, intraId.toString());
         SecurityContextHolder.clearContext();
-        return authUser.getIntraName();
+        return intraId;
     }
 }
