f9319c7ec4b0e69ebc89a58a9e37fc042246d736
parksooo <popomarin28@gmail.com>
2024-12-25 18:54:17 +0900

    [feat] : redis에 refreshToken 저장, 조회, 삭제 및 blcakList에 refreshToken 추가, 무효화하는 로직 추가

--- src/main/java/kr/where/backend/redisToken/RedisTokenService.java
+++ src/main/java/kr/where/backend/redisToken/RedisTokenService.java
@@ -1,6 +1,10 @@
 package kr.where.backend.redisToken;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import kr.where.backend.auth.filter.JwtConstants;
+import kr.where.backend.jwt.exception.JwtException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -9,6 +13,7 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class RedisTokenService {
     private final RedisTemplate<String, Object> redisTemplate;
     @Value("${accesstoken.expiration.time}")
@@ -16,55 +21,11 @@ public class RedisTokenService {
     @Value("${refreshtoken.expiration.time}")
     private long refreshTokenExpirationTime;
 
-    public RedisTokenService(RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
-    public void saveToken(final String intraId, final String accessToken, final String refreshToken) {
-        // Access Token 저장
-        redisTemplate.opsForValue().set("accessToken:" + intraId, accessToken, accessTokenExpirationTime, TimeUnit.MILLISECONDS);
-        // Refresh Token 저장
-        redisTemplate.opsForValue().set("refreshToken:" + intraId, refreshToken, refreshTokenExpirationTime, TimeUnit.MILLISECONDS);
-    }
-
-    public void saveAccessToken(final String intraId, final String accessToken) {
-        // Access Token 저장
-        redisTemplate.opsForValue()
-                .set(
-                        JwtConstants.ACCESS.getValue() + ":" + intraId,
-                        accessToken,
-                        accessTokenExpirationTime,
-                        TimeUnit.MILLISECONDS
-                );
-    }
-
     public void saveRefreshToken(final String intraId, final String refreshToken) {
         // Refresh Token 저장
         redisTemplate.opsForValue()
                 .set(
                          JwtConstants.REFRESH.getValue() + ":" + intraId,
-                        refreshToken,
-                        refreshTokenExpirationTime,
-                        TimeUnit.MILLISECONDS
-                );
-    }
-
-    public void saveInBlackList(final String intraId) {
-        final String accessToken = getAccessToken(intraId);
-        final String refreshToken = getRefreshToken(intraId);
-
-
-        redisTemplate.opsForValue()
-                .set(
-                        "expiredAccess:" + intraId,
-                        accessToken,
-                        accessTokenExpirationTime,
-                        TimeUnit.MILLISECONDS
-                );
-
-        redisTemplate.opsForValue()
-                .set(
-                        "expiredRefresh:" + intraId,
                         refreshToken,
                         refreshTokenExpirationTime,
                         TimeUnit.MILLISECONDS
@@ -79,21 +40,39 @@ public class RedisTokenService {
         return Objects.equals(token, redisTemplate.opsForValue().get("expiredRefresh:" + intraId));
     }
 
-    public String getAccessToken(final String intraId) {
-        return (String) redisTemplate.opsForValue().get(JwtConstants.ACCESS.getValue() + ":" + intraId);
-    }
-
     public String getRefreshToken(final String intraId) {
-        return (String) redisTemplate.opsForValue().get(JwtConstants.REFRESH.getValue() + ":" + intraId);
+        final String refreshToken = (String) redisTemplate.opsForValue()
+                .get(JwtConstants.REFRESH.getValue() + ":" + intraId);
+        if (Objects.equals(refreshToken, "null")) {
+            throw new JwtException.ExpiredJwtToken();
+        }
+        return refreshToken;
     }
 
-    public void deleteTokens(final String intraId) {
-        redisTemplate.delete(JwtConstants.ACCESS.getValue() + ":" + intraId);
+    public void invalidateToken(final String accessToken, final String intraId) {
+        saveInBlackList(accessToken, intraId);
+        deleteRefreshTokens(intraId);
+    }
+
+    private void saveInBlackList(final String accessToken, final String intraId) {
+        final String refreshToken = getRefreshToken(intraId);
+        redisTemplate.opsForValue()
+                .set(
+                        "expiredAccess:" + intraId,
+                        accessToken,
+                        accessTokenExpirationTime,
+                        TimeUnit.MILLISECONDS
+                );
+        redisTemplate.opsForValue()
+                .set(
+                        "expiredRefresh:" + intraId,
+                        refreshToken,
+                        refreshTokenExpirationTime,
+                        TimeUnit.MILLISECONDS
+                );
+    }
+
+    private void deleteRefreshTokens(final String intraId) {
         redisTemplate.delete(JwtConstants.REFRESH.getValue() + ":" + intraId);
-    }
-
-    public boolean isTokenExists(String intraId, String token) {
-        return token.equals(redisTemplate.opsForValue().get("accessToken:" + intraId)) ||
-                token.equals(redisTemplate.opsForValue().get("refreshToken:" + intraId));
     }
 }
