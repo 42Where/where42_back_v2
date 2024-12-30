eb1ef9c5a8507af45cb0a70e4d2ff51d2b38d0e0
parksooo <popomarin28@gmail.com>
2024-12-30 16:25:07 +0900

    [delete] : 개행 삭제

--- src/main/java/kr/where/backend/jwt/JwtController.java
+++ src/main/java/kr/where/backend/jwt/JwtController.java
@@ -1,8 +1,6 @@
 package kr.where.backend.jwt;
 
 import jakarta.servlet.http.HttpServletResponse;
-import kr.where.backend.auth.authUser.AuthUser;
-import kr.where.backend.auth.authUser.AuthUserInfo;
 import kr.where.backend.jwt.dto.RequestReissueDTO;
 import kr.where.backend.jwt.dto.ResponseAccessTokenDTO;
 import kr.where.backend.jwt.swagger.JwtApiDocs;
@@ -20,7 +18,6 @@ public class JwtController implements JwtApiDocs {
     public ResponseEntity<ResponseAccessTokenDTO> reIssue(final HttpServletResponse response,
                                                           @RequestBody final RequestReissueDTO requestReissueDTO)
     {
-
         return ResponseEntity.ok(
                 jwtService.reissueAccessToken(
                                 response,
