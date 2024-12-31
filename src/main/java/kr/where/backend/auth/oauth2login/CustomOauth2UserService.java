77b11b061889a7783e91dcf454d0df7608d4d709
soohalee <suha630@naver.com>
2024-12-31 15:24:35 +0900

    [refactor] : Delete wildcard used in import

--- src/main/java/kr/where/backend/auth/oauth2login/CustomOauth2UserService.java
+++ src/main/java/kr/where/backend/auth/oauth2login/CustomOauth2UserService.java
@@ -1,5 +1,8 @@
 package kr.where.backend.auth.oauth2login;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 import kr.where.backend.member.Member;
 import kr.where.backend.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +14,6 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
-import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Service
