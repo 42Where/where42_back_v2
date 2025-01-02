f8363782c1d378737b72700b0b81849522247178
parksooo <popomarin28@gmail.com>
2025-01-02 17:13:47 +0900

    [feat] : visualize requestBody for using api and add validation annotation

--- src/main/java/kr/where/backend/jwt/swagger/JwtApiDocs.java
+++ src/main/java/kr/where/backend/jwt/swagger/JwtApiDocs.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import kr.where.backend.jwt.dto.RequestReissueDTO;
 import kr.where.backend.jwt.dto.ResponseAccessTokenDTO;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "jwt", description = "jwt API")
 public interface JwtApiDocs {
     @Operation(
-            summary = "reissue accessToken of Expired time API",
-            description = "accessToken을 재 발급",
+            summary = "reissue accessToken of Expired time API", description = "accessToken을 재 발급",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(implementation = RequestReissueDTO.class))
+            ),
             responses = {
-                    @ApiResponse(responseCode = "200", description = "토큰 재발급 성공", content = @Content(schema = @Schema(implementation = ResponseAccessTokenDTO.class))),
+                    @ApiResponse(responseCode = "200", description = "토큰 재발급 성공",
+                            content = @Content(schema = @Schema(implementation = ResponseAccessTokenDTO.class))),
             }
 
     )
     @PostMapping("/reissue")
     ResponseEntity<ResponseAccessTokenDTO> reIssue(final HttpServletResponse response,
-                                                   @RequestBody final RequestReissueDTO requestReissueDTO);
+                                                   @RequestBody @Valid final RequestReissueDTO requestReissueDTO);
 }
