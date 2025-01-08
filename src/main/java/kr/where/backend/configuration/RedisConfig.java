923b3681d2fade7eaea6de25d23a7cb01ec572b6
parksooo <popomarin28@gmail.com>
2025-01-08 16:23:44 +0900

    [hotfix] : connect to redis with password

--- src/main/java/kr/where/backend/configuration/RedisConfig.java
+++ src/main/java/kr/where/backend/configuration/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -20,9 +21,14 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String passWord;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setPassword(passWord);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
