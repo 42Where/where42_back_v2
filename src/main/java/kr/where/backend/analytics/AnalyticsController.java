f35f73df96db7e15326974f2e1bd12cc5696318a
parksooo <popomarin28@gmail.com>
2025-01-24 16:30:38 +0900

    [feat] : responseEntity with view table

--- src/main/java/kr/where/backend/analytics/AnalyticsController.java
+++ src/main/java/kr/where/backend/analytics/AnalyticsController.java
@@ -1,10 +1,11 @@
 package kr.where.backend.analytics;
 
-import kr.where.backend.analytics.dto.ResponsePopularImacListDTO;
-import kr.where.backend.analytics.dto.ResponseSeatHistoryListDTO;
+import kr.where.backend.analytics.dto.ResponseAnalyticsDTO;
 import kr.where.backend.analytics.swagger.AnalyticsApiDocs;
 import kr.where.backend.aspect.LogLevel;
 import kr.where.backend.aspect.RequestLogging;
+import kr.where.backend.auth.authUser.AuthUser;
+import kr.where.backend.auth.authUser.AuthUserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestLogging(level = LogLevel.INFO)
 @RequestMapping("v3/analytics")
 public class AnalyticsController implements AnalyticsApiDocs {
+    private final AnalyticsService analyticsService;
 
     @Override
     @GetMapping("/seat-history")
-    public ResponseEntity<ResponseSeatHistoryListDTO> getMemberSeatHistory(@RequestParam("count") final Integer count) {
-        return null;
+    public ResponseEntity<ResponseAnalyticsDTO> getMemberSeatHistory(@AuthUserInfo final AuthUser authUser,
+                                                                     @RequestParam("count") final Integer count) {
+        return ResponseEntity.ok(
+                analyticsService.getMemberImacUsageAnalyticsData(authUser.getIntraId(), count)
+        );
     }
 
     @Override
     @GetMapping("/popular-imac")
-    public ResponseEntity<ResponsePopularImacListDTO> getPopularImac(@RequestParam("count") final Integer count) {
-        return null;
+    public ResponseEntity<ResponseAnalyticsDTO> getPopularImac(@AuthUserInfo final AuthUser authUser,
+                                                               @RequestParam("count") final Integer count) {
+        return ResponseEntity.ok(
+                analyticsService.getImacUsageAnalyticsData(count)
+        );
     }
 }
