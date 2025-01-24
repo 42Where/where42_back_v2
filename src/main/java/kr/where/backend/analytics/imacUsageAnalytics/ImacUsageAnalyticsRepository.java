b2878e826c47d7412534c32b24fb6f16377c4f3d
parksooo <popomarin28@gmail.com>
2025-01-24 16:28:52 +0900

    [feat] : find all imac view table

---
+++ src/main/java/kr/where/backend/analytics/imacUsageAnalytics/ImacUsageAnalyticsRepository.java
@@ -0,0 +1,13 @@
+package kr.where.backend.analytics.imacUsageAnalytics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ImacUsageAnalyticsRepository extends JpaRepository<ImacUsageAnalyticsView, Long> {
+    @Query("SELECT imacData FROM ImacUsageAnalyticsView imacData ORDER BY imacData.usageCount DESC")
+    List<ImacUsageAnalyticsView> findAllOrOrderByUsageCount();
+}
