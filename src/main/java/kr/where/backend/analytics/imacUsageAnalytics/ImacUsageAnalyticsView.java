c1ad2fdbc7ee1ef08894d0f3b407cb0829baaa73
parksooo <popomarin28@gmail.com>
2025-01-24 16:27:44 +0900

    [feat] : view table using imacHistory entity with native query (imac seat, using count, calculate using time)

---
+++ src/main/java/kr/where/backend/analytics/imacUsageAnalytics/ImacUsageAnalyticsView.java
@@ -0,0 +1,32 @@
+package kr.where.backend.analytics.imacUsageAnalytics;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Immutable
+@Subselect("SELECT ROW_NUMBER() OVER (ORDER BY imac) AS id, " +
+        "imac," +
+        "CAST(SUM(EXTRACT(EPOCH FROM logout_at - login_at)) AS BIGINT) AS usage_time, " +
+        "COUNT(imac) as usage_count, " +
+        "MIN(login_at) as login_at_first, " +
+        "MAX(logout_at) as logout_at_last " +
+        "FROM imac_history " +
+        "WHERE created_at >= CURRENT_TIMESTAMP - INTERVAL '7' DAY " +
+        "GROUP BY imac"
+)
+@Table(name = "imac_usage_analytics_view")
+@Getter
+public class ImacUsageAnalyticsView {
+    @Id
+    private Long id;
+    private String imac;
+    private Long usageTime;
+    private Long usageCount;
+    private LocalDateTime loginAtFirst;
+    private LocalDateTime logoutAtLast;
+}
