2dba7527fbbeb0971939514efb494a8b83779094
parksooo <popomarin28@gmail.com>
2025-01-30 16:39:44 +0900

    [fix] : to get imac_histories when create time in last week

--- src/main/java/kr/where/backend/analytics/memberImacUsageAnalytics/MemberImacUsageAnalyticsView.java
+++ src/main/java/kr/where/backend/analytics/memberImacUsageAnalytics/MemberImacUsageAnalyticsView.java
@@ -1,9 +1,5 @@
 package kr.where.backend.analytics.memberImacUsageAnalytics;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
@@ -22,23 +18,20 @@ import java.time.LocalDateTime;
         "MIN(login_at) AS login_at_first, " +
         "MAX(logout_at) AS logout_at_last " +
         "FROM imac_history " +
-        "WHERE created_at >= CURRENT_TIMESTAMP - INTERVAL '7' DAY " +
-        "GROUP BY imac, intra_id")
+        "WHERE created_at BETWEEN DATE_TRUNC('week', CURRENT_TIMESTAMP - INTERVAL '7' DAY) AND " +
+        "DATE_TRUNC('week', CURRENT_TIMESTAMP - INTERVAL '7' DAY) + INTERVAL '6' DAY " +
+        "GROUP BY imac, intra_id"
+)
 @Table(name = "member_imac_usage_analytics_view")
 @Getter
+@ToString
 public class MemberImacUsageAnalyticsView {
     @Id
     private Long id;
-
     private Integer intraId;
-
     private String imac;
-
     private Long usageTime;
-
     private Long usageCount;
-
     private LocalDateTime loginAtFirst;
-
     private LocalDateTime logoutAtLast;
 }
