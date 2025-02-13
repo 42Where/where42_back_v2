4f267fc4c8b449bb58bd76c4d55658dec5b8cc0e
parksooo <popomarin28@gmail.com>
2025-02-13 21:29:52 +0900

    [chore] : delete unused import value

--- src/main/java/kr/where/backend/configuration/SwaggerConfig.java
+++ src/main/java/kr/where/backend/configuration/SwaggerConfig.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.List;
 
 @OpenAPIDefinition(
